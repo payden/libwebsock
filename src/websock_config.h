0946ba1b700a8dd3524eb5d30c853b9a78df8b4e
Yarvis Hatsa <PoliceTerror@dyne.org>
2016-10-15 17:31:20 +0100

    added libwebsock pkg-config and fixed a compile error.

--- src/websock_config.h
+++ src/websock_config.h
@@ -2,8 +2,6 @@
 #ifndef WEBSOCK_CONFIG_H
 #define WEBSOCK_CONFIG_H 1
 
-#define LIBWEBSOCK_DEBUG 0
-
 #define WEBSOCK_PACKAGE_STRING "libwebsock 1.0.7"
 #define WEBSOCK_PACKAGE_VERSION "1.0.7"
 #define WEBSOCK_PACKAGE_NAME "libwebsock"
